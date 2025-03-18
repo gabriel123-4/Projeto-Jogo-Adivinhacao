492371cc853fa66445e1225f793802086f63dc59
gabriel123-4 <gabrielgoncalvespachco@gmail.com>
2025-03-18 15:07:40 -0300

    Add files via upload

---
+++ Atividade-jogo-adivinhaçao.py
@@ -0,0 +1,32 @@
+
+#limpeza da tela
+import os
+
+os.system("cls")
+
+
+#sistema pra pc escolher um número
+import os
+
+lista = [10, 20, 30, 40, 50]
+for lista in range(5):
+    print(lista, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Jogador
